586ebd49169f631f16ca864785c2e08d578e2697
Paul Donnelly <paul.donnelly.91@gmail.com>
2018-10-27 21:29:38 +0100

    weapon attacks and rolls with (dis)advantage

--- GameMechanics/Dice/d20.cs
+++ GameMechanics/Dice/d20.cs
@@ -7,5 +7,20 @@ namespace GameMechanics.Dice
     public class d20 : Die
     {
         public override int Sides { get { return 20; } }
+
+        public int Roll(bool hasAdvantage = false, bool hasDisadvantage = false)
+        {
+            var roll1 = Roll();
+            var roll2 = Roll();
+            if(hasAdvantage && !hasDisadvantage)
+            {
+                return Math.Max(roll1, roll2);
+            }
+            if(!hasAdvantage && hasDisadvantage)
+            {
+                return Math.Min(roll1, roll2);
+            }
+            return Roll();
+        }
     }
 }
